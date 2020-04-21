d15338ebe562938af3052cd8f7b0f3bb08987d10
Fran <frna.vlz@gmail.com>
2020-04-21 16:13:26 -0700

    npc

---
+++ Assets/Scripts/DialogTextBox.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogTextBox : MonoBehaviour
+{
+    [SerializeField]
+    Text dialog;
+    [SerializeField]
+    string message;
+
+    IEnumerator animateText;
+    [SerializeField, Range(0.01f, 4f)]
+    float animationSpeed = 1;
+
+    public string Message { get => message; set => message = value; }
+
+    public void ShowDialog()
+    {
+        animateText = Animate(animationSpeed);
+        StartCoroutine(animateText);
+    }
+
+    public void ClearText()
+    {
+        dialog.text = "";
+    }
+
+    IEnumerator Animate(float time)
+    {
+        for(int i=0; i < message.Length; i++) {
+            dialog.text += message[i];
+            yield return new WaitForSeconds(time);
+        }
+    }
+}
